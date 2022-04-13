e522a49f650d35aee42fba38a3f11bf8518d32f1
hcarter20 <hcarter@andrew.cmu.edu>
2022-04-13 17:50:55 -0400

    Added sounds to project

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    // List of all sounds played in the game
+    public Sound[] sounds;
+
+    // Singleton declaration, used by other scripts
+    public static AudioManager S;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (S == null)
+            S = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Stop();
+    }
+}
