8cee844b7c2b307648c970868caafd0eb1a6164c
Simon Corpuz <scorpuz@andrew.cmu.edu>
2022-04-19 23:25:44 -0400

    AAAAAAAAA

--- Assets/Scripts/EnvironmentTrigger.cs
+++ Assets/Scripts/EnvironmentTrigger.cs
@@ -20,12 +20,12 @@ public class EnvironmentTrigger : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (tag == "SoundTrigger" && collision.gameObject.CompareTag("Noisy"))
+        if (tag == "SoundTrigger" && collision.gameObject.CompareTag("Noisy") && GameManager.S.gameState == GameState.sleeping)
         {
             Debug.Log("Princess was woken up by noise from " + collision.gameObject.name);
             princess.WakeUp();
         }
-        if (tag == "TempTrigger" && collision.gameObject.CompareTag("Cold"))
+        if (tag == "TempTrigger" && collision.gameObject.CompareTag("Cold") && GameManager.S.gameState == GameState.sleeping)
         {
             Debug.Log("Princess was woken up by chills from " + collision.gameObject.name);
             princess.WakeUp();
